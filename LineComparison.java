e37de21e0f58d06313db75faa9948ca7c3420a4c
shital gawade <shitalgawade131@gmail.com>
2023-01-18 17:34:30 +0530

    Equal

--- LineComparison.java
+++ LineComparison.java
@@ -8,12 +8,20 @@ public class LineComparison
     }
     int x1 = 1, x2 = 4;         //point 1=(x1,y1)=(1,1)
     int y1 = 1, y2 = 4;         //point 2=(x2,y2)=(4,4)
-    double len;
+    int x3 = 5, y3 = 5;        //point 3 =(x3,y3)=(5,5)
+    Double len1,len2;
 
     void length() {
-        len  = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("length is:-"+len);
+        len1  = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("length between point 1 and 2:-"+len1);
+
+        len2 = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+        System.out.println("length between point2 and 3:-"+len2);
+
+        System.out.println(len1 + " .equals " + len2 + ": " + len1.equals(len2));
 
     }
-
 }
+
+
+
