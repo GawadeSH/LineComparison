f461b51c02da286886cb3d5668b42afd9ab9d465
shital gawade <shitalgawade131@gmail.com>
2023-01-18 17:28:32 +0530

    Length

--- LineComparison.java
+++ LineComparison.java
@@ -3,5 +3,17 @@ public class LineComparison
     public static void main(String[] args) {
 
         System.out.println("Welcome to Line Comparison Program");
+        LineComparison l=new LineComparison();
+        l.length();
     }
+    int x1 = 1, x2 = 4;         //point 1=(x1,y1)=(1,1)
+    int y1 = 1, y2 = 4;         //point 2=(x2,y2)=(4,4)
+    double len;
+
+    void length() {
+        len  = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("length is:-"+len);
+
+    }
+
 }
